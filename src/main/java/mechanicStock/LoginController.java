e2222de8f0fac370af1038bb04093480195d2b13
jack-bkr <jackbarker04@icloud.com>
2024-05-03 01:54:20 +0100

    Make all buttons open new windows from their own .fxml files

--- src/main/java/mechanicStock/LoginController.java
+++ src/main/java/mechanicStock/LoginController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.*;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoginController {
--- src/main/java/mechanicStock/MainAppController.java
+++ src/main/java/mechanicStock/MainAppController.java
@@ -8,10 +8,60 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class MainAppController {
-    @FXML Label welcomeLabel;
+    @FXML 
+    Label welcomeLabel;
     
     public void recieveUsername(String username) {
         welcomeLabel.setText("Welcome, " + username + "!");
     }
+
+    public void newScene(String fxmlString, int width, int height) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(fxmlString));
+            Parent root = loader.load();
+            Scene changeScene = new Scene(root, width, height);
+            changeScene.getStylesheets().add(getClass().getClassLoader().getResource("Main.css").toExternalForm());
+            stage.setScene(changeScene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML 
+    protected void handleLogoutButton(ActionEvent event) {
+        newScene("Login.fxml", 600, 400);
+    }
     
+    @FXML 
+    protected void handleViewPartsButton(ActionEvent event) {
+        newScene("ViewParts.fxml", 900, 500);
+    }
+    
+    @FXML
+    protected void handleViewVehiclesButton(ActionEvent event) {
+        newScene("ViewVehicles.fxml", 900, 500);
+    }
+    
+    @FXML
+    protected void handleViewUsersButton(ActionEvent event) {
+        newScene("ViewUsers.fxml", 900, 500);
+    }
+    
+    @FXML
+    protected void handlePrintAllButton(ActionEvent event) {
+
+    }
+    
+    @FXML
+    protected void handleAddStockButton(ActionEvent event) {
+        newScene("AddStock.fxml", 400, 600);
+    }
+    
+    @FXML
+    protected void handleAddUserButton(ActionEvent event) {
+        newScene("AddUsers.fxml", 400, 600);
+    }
+
 }
