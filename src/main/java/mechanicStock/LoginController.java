1cfbcd2cd26a37c7c7da536a4c8ed15b604a110e
jack-bkr <jackbarker04@icloud.com>
2024-05-03 16:27:18 +0100

    get login page to read from DB and output to console

--- src/main/java/mechanicStock/LoginController.java
+++ src/main/java/mechanicStock/LoginController.java
@@ -6,12 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class LoginController {
     @FXML private Button loginButton;
     @FXML private TextField userNameTextField;
 
-    @FXML protected void handleLoginButtonAction(ActionEvent event) throws Exception {
+    @FXML
+    protected void handleLoginButtonAction(ActionEvent event) throws Exception {
         Stage stage = (Stage) loginButton.getScene().getWindow();
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainApp.fxml"));
         Parent root = loader.load();
@@ -22,6 +24,56 @@ public class LoginController {
         Scene changeScene = new Scene(root, 900, 500);
         changeScene.getStylesheets().add(getClass().getClassLoader().getResource("Main.css").toExternalForm());
         stage.setScene(changeScene);
-        stage.show();        
+        stage.show();
+    }
+
+    @FXML
+    protected void handleGetLoginInfoButtonAction(ActionEvent event) {
+        getLogin();
+    }
+    
+    public static void getLogin() {
+        String dbURL = "jdbc:sqlite::resource:mechanicStockDB.sqlite";
+        String query = "SELECT * FROM Users;";
+        Connection conn = null;
+        Statement stmt = null;
+        
+        try {
+            System.out.println("Loading JDBC driver...");
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("JDBC driver loaded.");
+
+            conn = DriverManager.getConnection(dbURL);
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+                System.out.println(rs.getBoolean(4));
+                System.out.println(rs.getString(5));
+            }
+
+            stmt.close();
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Closing the connection.");
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                conn = null;
+            }
+        }
     }
 }
